02999a5ca47a434dbd29ef12e1049a88aa17c917
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-15 10:36:31 -0500

    Update cave.cpp

--- cave.cpp
+++ cave.cpp
@@ -1,8 +1,20 @@
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
 #include <iostream>
 using namespace std;
 static int SEED = 65536; //test seed
+int sphere(x,y,z,r) {
+    int xx = 0, yy = 0, zz = 0;
+    for (xx = 0 - r; xx < r + 1; xx++) {
+        for (yy = 0 - r; yy < r + 1; yy++) {
+            for (zz = 0 - r; zz < r + 1; zz++) {
+                //TODO
+            }
+        }
+    }
+    return 0;
+}
 int main() {
     srand((unsigned)SEED);
     int radius;
