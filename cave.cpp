0bcfdcd40d9ff268f7e84507d282df1323701077
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-15 10:45:01 -0500

    Update cave.cpp

--- cave.cpp
+++ cave.cpp
@@ -1,15 +1,17 @@
 #include <cstdlib>
-#include <ctime>
 #include <cmath>
 #include <iostream>
 using namespace std;
 static int SEED = 65536; //test seed
 int sphere(x,y,z,r) {
     int xx = 0, yy = 0, zz = 0;
+    int pos[3] = [x,y,z]
     for (xx = 0 - r; xx < r + 1; xx++) {
         for (yy = 0 - r; yy < r + 1; yy++) {
             for (zz = 0 - r; zz < r + 1; zz++) {
-                //TODO
+                pos[0] = x + xx;
+                pos[1] = y + yy;
+                pos[2] = z + zz;
             }
         }
     }
