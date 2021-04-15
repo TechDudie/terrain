1126eafca06e065b2745a6b758aada1731081659
TechDudie <73961295+TechDudie@users.noreply.github.com>
2021-04-15 09:35:34 -0500

    Update cave.cpp

--- cave.cpp
+++ cave.cpp
@@ -1,1 +1,13 @@
-
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+static int SEED = 65536; //test seed
+int main() {
+    srand((unsigned)SEED);
+    int radius;
+    radius = (rand()%8)+8;
+    cout << radius << "\n";
+    // TODO SPHERE GEN
+    return 0;
+}
